d13ec792923068792f9b28414335ae67d5c99b03
Omkar Desai <omkar.desai916@gmail.com>
2022-08-04 17:14:54 +0530

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
