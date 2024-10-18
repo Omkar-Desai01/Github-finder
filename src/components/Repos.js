eaa0df8e0d3affa8fba4e1336631386d589cc201
Omkar <omkar.desai916@gmail.com>
2024-10-19 00:44:00 +0530

    optimise

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,7 +2,7 @@ import React from "react";
 import Repo from "./Repo";
 
 export default function Repos(props) {
-  let allRepos = props.currentRepo.map(function (repo) {
+  let allRepos = props.currentRepo.map((repo) => {
     return <Repo data={repo} key={repo.id} />;
   });
   return (
